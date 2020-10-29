3e6f5f1521434b51203890ae7b165c43c9729b7e
gnamiro <naeimghahramanpour@gmail.com>
2020-10-29 20:18:10 +0330

    mergin master and main

--- Reg.v
+++
@@ -1,8 +0,0 @@
-module Reg #(parameter n =1)(input clk,ld,[n-1:0]in,output[n-1:0]Out);
-    reg [n-1:0] out;
-    always @(posedge clk)begin 
-        if(ld)
-            out <= in;
-    end
-    assign Out=out;
-endmodule 
